035810e1325f89f3923a14f9a8f445f665e9360c
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-09-24 12:13:26 +0800

    Feat: Add get all posts controller

--- controllers/postsController.ts
+++ controllers/postsController.ts
@@ -1,9 +1,28 @@
-import { create } from "domain";
 import asyncHandler from "express-async-handler";
+import { Request, Response } from "express";
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const postsController = {
-  index: asyncHandler(async (req, res) => {
-    // TODO: GET ALL POSTS
+  index: asyncHandler(async (req: Request, res: Response) => {
+    const userRole = req.user?.role;
+
+    // If user is an admin, return all posts
+    if (userRole === "ADMIN") {
+      const posts = await prisma.post.findMany();
+      res.status(200).json(posts);
+      return;
+    }
+
+    // If user is not an admin, return only published posts
+    const posts = await prisma.post.findMany({
+      where: {
+        published: true,
+      },
+    });
+
+    res.status(200).json(posts);
   }),
 
   getPostById: asyncHandler(async (req, res) => {
