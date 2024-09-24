d1cc950864fbd6211234c5e243e37fb895c4b81b
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-09-24 12:30:38 +0800

    Feat: Add getPostById controller

--- controllers/postsController.ts
+++ controllers/postsController.ts
@@ -25,8 +25,34 @@ const postsController = {
     res.status(200).json(posts);
   }),
 
-  getPostById: asyncHandler(async (req, res) => {
-    // TODO: GET POST BY ID
+  getPostById: asyncHandler(async (req: Request, res: Response) => {
+    const userRole = req.user?.role;
+    const postId = req.params.id;
+
+    const post = await prisma.post.findUnique({
+      where: {
+        id: postId,
+      },
+    });
+
+    // Not exists
+    if (!post) {
+      res.status(404).json({ msg: "Post not found" });
+      return;
+    }
+
+    // ADMIN -> Can access all posts
+    // USER -> Cannot access unpublished posts
+
+    // Cannot access if not an ADMIN and post is unpublished
+    if (userRole === "USER" && !post.published) {
+      res
+        .status(403)
+        .json({ msg: "Forbidden: You do not have access to this post." });
+      return;
+    }
+
+    res.status(200).json(post);
   }),
 
   createPost: asyncHandler(async (req, res) => {
