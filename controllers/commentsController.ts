f2adf827a3b6af0ac0583c75315531251e0fb9a0
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-09-24 13:58:09 +0800

    Feat: Add getAllComments functionality

--- controllers/commentsController.ts
+++ controllers/commentsController.ts
@@ -1,8 +1,19 @@
 import asyncHandler from "express-async-handler";
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const commentsController = {
-  getAllComments: asyncHandler(async (req, res) => {
-    // TODO: GET ALL COMMENTS
+  getAllComments: asyncHandler(async (req: Request, res: Response) => {
+    const postId = req.params.postId;
+
+    const comments = await prisma.comment.findMany({
+      where: {
+        postId: postId,
+      },
+    });
+
+    res.status(200).json(comments);
   }),
 
   getCommentById: asyncHandler(async (req, res) => {
